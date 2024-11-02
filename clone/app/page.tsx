288d4e61f965dd73852b9c78d9cd67578cca5ed8
rChwiecko <ryanchwiecko10@gmail.com>
2024-11-01 23:28:36 -0400

    adding convex

--- clone/app/page.tsx
+++ clone/app/page.tsx
@@ -1,5 +1,43 @@
+"use client";
+
+import { useState } from "react";
+
+interface Message {
+  sender: string;
+  content: string;
+}
+
 export default function Home() {
+  const [messages, setMessages] = useState<Message[]>([
+    { sender: "Alice", content: "Hello World" },
+    { sender: "Bob", content: "Hi Alice!" },
+  ]);
+
+  const [input, setInput] = useState("");
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setMessages([...messages, { sender: "Alice", content: input }]);
+    setInput("");
+  };
+
   return (
-    <div>Hello world</div>
+    <div>
+      {messages.map((message, index) => (
+        <div key={index}>
+          <strong>{message.sender}</strong>: {message.content}
+        </div>
+      ))}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="message"
+          id="message"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   );
 }
